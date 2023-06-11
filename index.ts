6dc271715543d31de1cb987d0a0fb3d2a7e287ad
Paul Evans <->
2023-06-11 22:07:52 +0100

    Make full height and remove shortcuts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import {PureComponent, createElement as $, RefObject, createRef} from 'react';
+import { PureComponent, createElement as $, RefObject, createRef } from "react";
 import {
   StyleSheet,
   Text,
@@ -10,8 +10,8 @@ import {
   TouchableOpacity,
   FlatList,
   TextInput,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 const noop = () => {};
 
 const justGroupBy = <T>(array: T[], keyFn: (item: T) => string) => {
@@ -23,9 +23,12 @@ const justGroupBy = <T>(array: T[], keyFn: (item: T) => string) => {
     result[key].push(item);
     return result;
   }, {});
-}
+};
 
-const justMapValues = <T>(obj: { [key: string]: T }, callback: (value: T) => any) => {
+const justMapValues = <T>(
+  obj: { [key: string]: T },
+  callback: (value: T) => any
+) => {
   const result: { [key: string]: any } = {};
 
   for (const key in obj) {
@@ -35,7 +38,7 @@ const justMapValues = <T>(obj: { [key: string]: T }, callback: (value: T) => any
   }
 
   return result;
-}
+};
 
 interface Emoji {
   category: string;
@@ -51,7 +54,7 @@ interface Emoji {
 // and `String.fromCodePoint` on MDN
 function charFromUtf16(utf16: string) {
   return String.fromCodePoint(
-    ...(utf16.split('-').map((u) => '0x' + u) as any),
+    ...(utf16.split("-").map((u) => "0x" + u) as any)
   );
 }
 
@@ -68,32 +71,32 @@ type LocalizedCategories = [
   string, // Travel & Places
   string, // Objects
   string, // Symbols
-  string, // Flags
+  string // Flags
 ];
 
 const CATEGORIES: LocalizedCategories = [
-  'Smileys & Emotion',
-  'People & Body',
-  'Animals & Nature',
-  'Food & Drink',
-  'Activities',
-  'Travel & Places',
-  'Objects',
-  'Symbols',
-  'Flags',
+  "Smileys & Emotion",
+  "People & Body",
+  "Animals & Nature",
+  "Food & Drink",
+  "Activities",
+  "Travel & Places",
+  "Objects",
+  "Symbols",
+  "Flags",
 ];
 
 function categoryToIcon(cat: string) {
-  if (cat === 'Smileys & Emotion') return 'emoticon';
-  if (cat === 'People & Body') return 'human-greeting';
-  if (cat === 'Animals & Nature') return 'cat';
-  if (cat === 'Food & Drink') return 'food-apple';
-  if (cat === 'Activities') return 'tennis-ball';
-  if (cat === 'Travel & Places') return 'car';
-  if (cat === 'Objects') return 'lightbulb';
-  if (cat === 'Symbols') return 'alert';
-  if (cat === 'Flags') return 'flag-variant';
-  return 'emoticon-cool';
+  if (cat === "Smileys & Emotion") return "emoticon";
+  if (cat === "People & Body") return "human-greeting";
+  if (cat === "Animals & Nature") return "cat";
+  if (cat === "Food & Drink") return "food-apple";
+  if (cat === "Activities") return "tennis-ball";
+  if (cat === "Travel & Places") return "car";
+  if (cat === "Objects") return "lightbulb";
+  if (cat === "Symbols") return "alert";
+  if (cat === "Flags") return "flag-variant";
+  return "emoticon-cool";
 }
 
 const DEFAULT_EMOJI_SIZE = 32;
@@ -101,21 +104,20 @@ const SHORTCUT_SIZE = DEFAULT_EMOJI_SIZE * 0.75;
 const SEARCH_ICON_SIZE = DEFAULT_EMOJI_SIZE * 0.625;
 const PADDING = 5;
 const DEFAULT_COLUMNS = 7;
-const ROWS_VISIBLE = DEFAULT_COLUMNS;
 const EMOJI_GROUP_PADDING_BOTTOM = PADDING * 3;
-const TOTAL_HEIGHT = DEFAULT_EMOJI_SIZE * ROWS_VISIBLE + PADDING * 2;
+const TOTAL_HEIGHT = "100%";
 
 const styles = StyleSheet.create({
   modal: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   background: {
-    backgroundColor: '#00000077',
-    position: 'absolute',
+    backgroundColor: "#00000077",
+    position: "absolute",
     top: 0,
     bottom: 0,
     left: 0,
@@ -124,10 +126,10 @@ const styles = StyleSheet.create({
   },
 
   container: {
-    backgroundColor: '#141414',
+    backgroundColor: "#141414",
     padding: 0,
     borderRadius: 10,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
 
   scrollerContainer: {
@@ -135,21 +137,21 @@ const styles = StyleSheet.create({
     maxHeight: TOTAL_HEIGHT,
     ...Platform.select({
       web: {
-        overflowY: 'scroll',
+        overflowY: "scroll",
       },
     }),
   },
 
   scroller: {
-    flexDirection: 'column',
+    flexDirection: "column",
     minHeight: TOTAL_HEIGHT,
     maxHeight: TOTAL_HEIGHT,
     paddingHorizontal: PADDING,
   },
 
   searchContainer: {
-    position: 'relative',
-    flexDirection: 'row',
+    position: "relative",
+    flexDirection: "row",
     paddingTop: PADDING,
     paddingHorizontalLeft: PADDING,
     paddingBottom: 2,
@@ -157,7 +159,7 @@ const styles = StyleSheet.create({
 
   search: {
     flex: 1,
-    backgroundColor: '#212121',
+    backgroundColor: "#212121",
     marginTop: 3,
     marginHorizontal: 3,
     height: 4 + 20 + 4,
@@ -165,12 +167,12 @@ const styles = StyleSheet.create({
     paddingLeft: 32,
     paddingRight: 12,
     borderRadius: 3,
-    color: '#979797',
+    color: "#979797",
     zIndex: 10,
   },
 
   searchIcon: {
-    position: 'absolute',
+    position: "absolute",
     left: 16,
     top: PADDING + 3 + 4,
     zIndex: 20,
@@ -178,27 +180,28 @@ const styles = StyleSheet.create({
 
   headerText: {
     padding: PADDING,
-    color: 'black',
-    fontWeight: 'bold',
-    justifyContent: 'center',
-    textAlignVertical: 'center',
+    color: "black",
+    fontWeight: "bold",
+    justifyContent: "center",
+    textAlignVertical: "center",
   },
 
   categoryOuter: {
-    flexDirection: 'column',
-    alignItems: 'flex-start',
+    flexDirection: "column",
+    alignItems: "flex-start",
   },
 
   emojiGroup: {
     marginBottom: EMOJI_GROUP_PADDING_BOTTOM,
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    flexDirection: 'row',
+    alignItems: "center",
+    flexWrap: "wrap",
+    flexDirection: "row",
   },
 
   shortcutsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    display: "hidden",
+    flexDirection: "row",
+    justifyContent: "space-between",
     padding: PADDING,
   },
 
@@ -220,9 +223,9 @@ class EmojiGroup extends PureComponent<{
     const style = {
       width: size,
       height: size,
-      boxSizing: 'content-box',
+      boxSizing: "content-box",
       fontSize: size * 0.8,
-      textAlign: 'center' as const,
+      textAlign: "center" as const,
       lineHeight: size,
       margin: PADDING,
     } as TextStyle;
@@ -232,7 +235,7 @@ class EmojiGroup extends PureComponent<{
 
     return $(
       View,
-      {style: [styles.emojiGroup, {minWidth, maxWidth}]},
+      { style: [styles.emojiGroup, { minWidth, maxWidth }] },
       ...emojis
         .filter((e) => !!e)
         .map((e) =>
@@ -243,9 +246,9 @@ class EmojiGroup extends PureComponent<{
               key: e,
               onPress: () => this.props.onEmojiSelected(e),
             },
-            e,
-          ),
-        ),
+            e
+          )
+        )
     );
   }
 }
@@ -279,15 +282,15 @@ class EmojiCategory extends PureComponent<{
 
     return $(
       View,
-      {style: styles.categoryOuter},
-      $(Text, {style: [styles.headerText, headerStyle]}, categoryText),
+      { style: styles.categoryOuter },
+      $(Text, { style: [styles.headerText, headerStyle] }, categoryText),
       $(EmojiGroup, {
         emojis,
         onEmojiSelected,
         emojiSize,
         emojiStyle,
         columns,
-      }),
+      })
     );
   }
 }
@@ -298,26 +301,26 @@ class SearchField extends PureComponent<{
   onChanged: (str: string) => void;
 }> {
   public render() {
-    const {customStyle, iconColor, onChanged} = this.props;
+    const { customStyle, iconColor, onChanged } = this.props;
     return $(
       View,
-      {style: styles.searchContainer},
+      { style: styles.searchContainer },
       $(Icon, {
-        key: 'a',
+        key: "a",
         size: SEARCH_ICON_SIZE,
         style: styles.searchIcon,
-        color: iconColor ?? '#bcbcbc',
-        name: 'magnify',
+        color: iconColor ?? "#bcbcbc",
+        name: "magnify",
       }),
       $(TextInput, {
-        key: 'b',
+        key: "b",
         style: [styles.search, customStyle],
         onChangeText: onChanged,
         autoFocus: false,
         multiline: false,
-        returnKeyType: 'search',
-        underlineColorAndroid: 'transparent',
-      }),
+        returnKeyType: "search",
+        underlineColorAndroid: "transparent",
+      })
     );
   }
 }
@@ -331,21 +334,26 @@ class CategoryShortcuts extends PureComponent<{
 }> {
   public render() {
     // Scroll doesn't work on react-native-web due to bad FlatList support
-    if (Platform.OS === 'web') {
-      return $(View, {style: styles.shortcutsContainer});
+    if (Platform.OS === "web") {
+      return $(View, { style: styles.shortcutsContainer });
     }
 
-    const {onPressCategory, iconColor, activeCategory, activeIconColor, show} =
-      this.props;
+    const {
+      onPressCategory,
+      iconColor,
+      activeCategory,
+      activeIconColor,
+      show,
+    } = this.props;
 
     return $(
       View,
-      {style: styles.shortcutsContainer},
+      { style: styles.shortcutsContainer },
       ...CATEGORIES.map((category) => {
         if (show) {
           return $(
             TouchableOpacity,
-            {onPress: () => onPressCategory?.(category)},
+            { onPress: () => onPressCategory?.(category) },
 
             $(Icon, {
               key: category,
@@ -353,10 +361,10 @@ class CategoryShortcuts extends PureComponent<{
               style: styles.shortcut,
               color:
                 category === activeCategory
-                  ? activeIconColor ?? '#0c0c0c'
-                  : iconColor ?? '#bcbcbc',
+                  ? activeIconColor ?? "#0c0c0c"
+                  : iconColor ?? "#bcbcbc",
               name: categoryToIcon(category),
-            }),
+            })
           );
         } else {
           return $(Icon, {
@@ -364,10 +372,10 @@ class CategoryShortcuts extends PureComponent<{
             size: SHORTCUT_SIZE,
             style: styles.shortcut,
             name: categoryToIcon(category),
-            color: 'transparent',
+            color: "transparent",
           });
         }
-      }),
+      })
     );
   }
 }
@@ -375,10 +383,10 @@ class CategoryShortcuts extends PureComponent<{
 function normalize(str: string) {
   return str
     .toLocaleLowerCase()
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .replace(/ +/g, '')
-    .replace(/_+/g, ' ')
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/ +/g, "")
+    .replace(/_+/g, " ")
     .trim();
 }
 
@@ -406,14 +414,14 @@ export default class EmojiModal extends PureComponent<
 > {
   constructor(props: any) {
     super(props);
-    this.state = {searchResults: [], activeCategory: CATEGORIES[0]};
+    this.state = { searchResults: [], activeCategory: CATEGORIES[0] };
     this.prepareEmojisByCategory();
     this.calculateLayouts(props);
   }
 
   private emojisByCategory: Record<string, Array<string>> = {};
   private filteredEmojis: Array<Emoji> = [];
-  private layouts: Array<{length: number; offset: number; index: number}>;
+  private layouts: Array<{ length: number; offset: number; index: number }>;
   private readonly ref: RefObject<FlatList<unknown>> = createRef();
   private readonly viewabilityConfig = {
     minimumViewTime: 1,
@@ -421,12 +429,12 @@ export default class EmojiModal extends PureComponent<
   };
 
   private prepareEmojisByCategory() {
-    const emojiDB = require('./emoji.json') as Array<Emoji>;
-    const blocklistedEmojis = ['white_frowning_face', 'keycap_star', 'eject'];
+    const emojiDB = require("./emoji.json") as Array<Emoji>;
+    const blocklistedEmojis = ["white_frowning_face", "keycap_star", "eject"];
 
     this.filteredEmojis = emojiDB.filter((emoji: Emoji) => {
       if (blocklistedEmojis.includes(emoji.short_name)) return false;
-      if (Platform.OS === 'android') {
+      if (Platform.OS === "android") {
         const addedIn = parseFloat(emoji.added_in);
         if (Number.isNaN(addedIn)) return true;
         if (addedIn < 2) return true;
@@ -442,15 +450,16 @@ export default class EmojiModal extends PureComponent<
 
     const groupedEmojis = justGroupBy(
       this.filteredEmojis,
-      (emoji: Emoji) => emoji.category,
+      (emoji: Emoji) => emoji.category
     );
 
-    this.emojisByCategory = justMapValues(groupedEmojis, (group: Array<Emoji>) =>
-      group.map(charFromEmojiObj),
+    this.emojisByCategory = justMapValues(
+      groupedEmojis,
+      (group: Array<Emoji>) => group.map(charFromEmojiObj)
     );
   }
 
-  private calculateLayouts(props: EmojiModal['props']) {
+  private calculateLayouts(props: EmojiModal["props"]) {
     let heightsSoFar = 0;
     this.layouts = CATEGORIES.map((category, i) => {
       const numEmojis = this.emojisByCategory[category].length;
@@ -463,14 +472,14 @@ export default class EmojiModal extends PureComponent<
       const bottomPadding = EMOJI_GROUP_PADDING_BOTTOM;
       const height = headerHeight + numRows * rowHeight + bottomPadding;
       heightsSoFar += height;
-      return {length: height, offset, index: i};
+      return { length: height, offset, index: i };
     });
   }
 
-  private renderItem = ({item}: any) => {
-    const {searchResults} = this.state;
+  private renderItem = ({ item }: any) => {
+    const { searchResults } = this.state;
     if (searchResults.length > 0) {
-      return $(EmojiGroup, {...this.props, emojis: searchResults});
+      return $(EmojiGroup, { ...this.props, emojis: searchResults });
     } else {
       const category = item;
       return $(EmojiCategory, {
@@ -485,7 +494,7 @@ export default class EmojiModal extends PureComponent<
   private onSearchChanged = (input: string) => {
     if (input.length === 0) {
       if (this.state.searchResults.length > 0) {
-        this.setState({searchResults: []});
+        this.setState({ searchResults: [] });
       }
       return;
     }
@@ -512,14 +521,14 @@ export default class EmojiModal extends PureComponent<
       .sort((a, b) => b._score - a._score)
       .map(charFromEmojiObj);
 
-    if (searchResults.length === 0) searchResults.push('');
+    if (searchResults.length === 0) searchResults.push("");
 
-    this.setState({searchResults});
+    this.setState({ searchResults });
   };
 
   private onPressCategory = (category: string) => {
     // Scroll doesn't work on react-native-web due to bad FlatList support
-    if (Platform.OS === 'web') return;
+    if (Platform.OS === "web") return;
 
     const index = CATEGORIES.indexOf(category);
     if (index >= 0) {
@@ -537,14 +546,15 @@ export default class EmojiModal extends PureComponent<
   };
 
   getItemLayout = (data: Array<unknown> | null | undefined, index: number) => {
-    if (data?.[0] === null) return {length: TOTAL_HEIGHT, offset: 0, index: 0};
+    if (data?.[0] === null)
+      return { length: TOTAL_HEIGHT, offset: 0, index: 0 };
     return this.layouts[index];
   };
 
-  onViewableItemsChanged = ({viewableItems}: any) => {
+  onViewableItemsChanged = ({ viewableItems }: any) => {
     if (viewableItems.length === 0) return;
     const category = viewableItems[0].key;
-    this.setState({activeCategory: category});
+    this.setState({ activeCategory: category });
   };
 
   public render() {
@@ -557,14 +567,14 @@ export default class EmojiModal extends PureComponent<
       shortcutColor,
       activeShortcutColor,
     } = this.props;
-    const {searchResults, activeCategory} = this.state;
+    const { searchResults, activeCategory } = this.state;
 
     return $(
       View,
-      {style: [styles.modal, modalStyle]},
+      { style: [styles.modal, modalStyle] },
       $(
         View,
-        {style: [styles.container, containerStyle]},
+        { style: [styles.container, containerStyle] },
         $(SearchField, {
           customStyle: searchStyle,
           onChanged: this.onSearchChanged,
@@ -572,13 +582,13 @@ export default class EmojiModal extends PureComponent<
         }),
         $(
           View,
-          {style: styles.scrollerContainer},
+          { style: styles.scrollerContainer },
           $(FlatList, {
-            ['ref' as any]: this.ref,
+            ["ref" as any]: this.ref,
             data: searchResults.length > 0 ? [null] : CATEGORIES,
             horizontal: false,
             numColumns: 1,
-            onEndReachedThreshold: Platform.OS === 'web' ? 1 : 1000,
+            onEndReachedThreshold: Platform.OS === "web" ? 1 : 1000,
             onScrollToIndexFailed: noop,
             style: [styles.scroller, scrollStyle],
             initialNumToRender: 1,
@@ -588,7 +598,7 @@ export default class EmojiModal extends PureComponent<
             onViewableItemsChanged: this.onViewableItemsChanged,
             viewabilityConfig: this.viewabilityConfig,
             renderItem: this.renderItem,
-          }),
+          })
         ),
         $(CategoryShortcuts, {
           show: searchResults.length === 0,
@@ -596,14 +606,14 @@ export default class EmojiModal extends PureComponent<
           iconColor: shortcutColor,
           activeIconColor: activeShortcutColor,
           onPressCategory: this.onPressCategory,
-        }),
+        })
       ),
 
       $(
         TouchableWithoutFeedback,
-        {onPress: this.onPressBackground},
-        $(View, {style: [styles.background, backgroundStyle]}),
-      ),
+        { onPress: this.onPressBackground },
+        $(View, { style: [styles.background, backgroundStyle] })
+      )
     );
   }
 }
